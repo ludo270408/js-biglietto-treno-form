614e3071f40090a84b756e63708d8a7d7eb3da51
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-28 23:03:14 +0100

    [UPTADE] Exercise done

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ button.addEventListener('click', function(){
     let km = document.getElementById('km').value;
     let age = document.getElementById('age').value;
 
-    if(){
+    if(age == 1){
+        price = 0.21 *km * km* 0.8;
     }
-    else if(){
-
+    else if(age == 3){
+        price = 0.21 *km * km* 0.6;
     }
     else{
-
+        price = 0.21 *km * km;
     }
 
     console.log(price);

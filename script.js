9bc09fab41a3c20a66a988c7c8f3707c37af2b30
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-28 22:37:25 +0100

    [UPTADE] Variables declared

---
+++ script.js
@@ -0,0 +1,7 @@
+button.addEventListener('click', function(){
+    let name = document.getElementById('name').value;
+    let km = document.getElementById('km').value;
+    let age = document.getElementById('age').value;
+
+
+};)

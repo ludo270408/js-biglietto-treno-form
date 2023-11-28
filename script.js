ed54f1c27980cd54cb74454665f1a3993362985c
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-28 22:53:22 +0100

    [UPTADE] Structure of the if done

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ button.addEventListener('click', function(){
     let km = document.getElementById('km').value;
     let age = document.getElementById('age').value;
 
+    if(){
+    }
+    else if(){
 
-};)
+    }
+    else{
+
+    }
+
+    console.log(price);
+});
